41d805bf8eb2a6cfb9fe87697627c69bb2a4eeed
seoyeeon <shift2013@naver.com>
2026-03-12 16:08:26 +0900

    문서객체모델 basic실습완료

---
+++ 02_Node/01_basic/01_nodejs기초/00_async/01_async.js
@@ -0,0 +1,6 @@
+function sayhello() {
+  console.log(`Hellow World!`);
+}
+
+setTimeout(sayhello, 3000);
+console.log(`안녕`);
---
+++ 02_Node/01_basic/01_nodejs기초/03_arrow.js
@@ -0,0 +1,26 @@
+// 화살표함수
+
+let getTriangle = (base, height) => {
+  return (base * height) / 2;
+};
+
+console.log('삼각형의 면적:' + getTriangle(5, 2));
+
+//
+const person = {
+  name: 'bear',
+
+  // 일반함수
+  hello1: function () {
+    console.log(`일반함수 : `, this);
+  },
+
+  // 화살표함수
+  hello2: () => {
+    console.log(`화살표함수 : `, this);
+  }
+};
+
+
+person.hello1();
+person.hello2();
